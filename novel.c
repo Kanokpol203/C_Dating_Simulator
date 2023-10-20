7b4d442c6a4a5914dc7e21efbde4f86fb1ee8d3a
Kanokpol203 <65070005@kmitl.ac.th>
2023-10-20 16:35:31 +0700

    Day 1 and intro changed
    It's better and gayer

--- novel.c
+++ novel.c
@@ -21,36 +21,59 @@ void delayprint(char string[],int milisecond){
     }
 }
 void intro(){
-    delayprint("In this game you will play as \"Un\" \n\
-he's a soy milk merchant who's gonna meet \"Kao\" \n\
-Who's a librarian who's a very delicate person\n\
-to play this game there will be decisions\n\
-that you have to make so choose wisely and try to get \n\
-the information as much as possible the choice will be like this\n\
-Do you under stand how to play?\n\
-([1:type 1 to continue] / [2:type 2 to read the intro again])\n", 2);
+    delayprint("Through one connection flickers another, through communication nurture their hearts.\n\
+\"Love you from the star\"\n\
+\n\
+This story are told through the diary of an individual. though the way it ends, \n\
+you are the one who make the choice. Will you be the one to brings them together, \n\
+or be the one who severs.\n\
+\n\
+Shall the story commence?\n\
+(y:Yes, n:No)", 2);
     while(1){
         scanf(" %c", &input);
-        if(input == '1'){
+        if(tolower(input) == 'y'){
             break;
-        }else if(input == '2'){
+        }else if(tolower(input) == 'n'){
+            printf("Why r u here?\n");
+            delay(1000);
             system("clear");
-            intro();
             break;
         }else{
             printf("Please type in your answer\n");
         }
     }
+    system("clear");
+    delayprint("Character Introduction:\n\
+Name: Kao\n\
+Age: 25\n\
+Occupation: Librarian\n\
+Daily routine: Buying a Soybean milk from a nearby local shop before getting to work\n\
+\n\
+Name: Oun\n\
+Age: 25\n\
+Occupation: Soybean milk vendor (Family business)\n\
+Daily routine: Selling Soybean milk during the morning\n\
+(previous goal was pocket money, present goal is meeting his romantic interest.)\n", 1);
+    delayprint("You are Oun, The Soybean milk vendor. Your only goal is getting a happy relationship with Kao, \n\
+possibly much more", 2);
+    delayprint(". . .\n", 50);
+    delay(2000);
 }
 void day1(){
     delayprint("DAY 1\n\
-Kao usually buys soy milks before heading to work.\n\
-Kao: 2 bags of soy milk please.\n\
-Un : Sure thing!\n\
-Kao: Your uncle isn't here today?\n\
-Un look to Kao and said \"Oh my dad? he let me inherit this shop after he's retired!\n\
-After he look to Kao his chest suddenly warm and his heart beating harder.\n\
-Kao: Ah I see! Kao smile", 2);
+A young librarian heads off through his usual path. Unwavering shadow of the tree.\n\
+the roaring distance noise of the car engine. the sounds of the motorcycle passing by. \n\
+All are the norms for kao. He leisurely enjoy the surroundings while having thoughts about his work. \n\
+A minutes passed, he arrived at the workplace. but instead of entering, he head towards a familiar location.\n\
+\"1 Soy milk alongside a pair of Youtiao please.\" spoken in a softtone voice. \n\
+\"Coming up.\" repiled by the vendor.\n\
+\"Uncle isn't here today?\" kao asked. \n\
+\"Pardon?\" the vendor shift his focus from preparing the order to the individual.\n\
+they made eye contact. \n\
+\"He was usually the one who does all work here, I was wondering where he been.\" kao questioned.\n\
+\"oh..he retired, I'm the one who take the job after my father.\" Oun replied with a smile on his face.\n\
+ \"I see, tell him his usual customer miss him.\" kao said with a satisfied smile. \"understood\".", 2);
 }
 void day2(){
 
@@ -76,16 +99,20 @@ int main(){
     day1();
     while(1){
         printf("What would you do?\n\
-([1: Ask his name]/[2: Just give him the soy milk])\n");
+([1: Ask the vendor his name]/[2: leave him be])\n");
         scanf(" %c", &input);
         if(input == '1'){
             system("clear");
-            delayprint("Kao and Un Introduce themself their bond has start from now", 2);
+            delayprint("by the way, may I know your name?\" Oun asked.\n\
+\"I'm kao\" replied by kao.\n\
+\"My name is Oun, good to know you. here is your order", 2);
             break;
         }else if(input == '2'){
-            system("clear");
+            delayprint("here is your order", 2);
+            delay(5000);
             gameend = 1;
-            printf("Bad Ending 1: you two are just customer and a merchant not even trying to flirt him\n");
+            system("clear");
+            printf("BAD END: Both of you remained just a customer and vendor to each other... Connection must be made to go further, and you failed to do so.\n");
             break;
         }else{
             system("clear");
